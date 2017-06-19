7a2a751f8e5a76b14e360bdd8b1766d10b701605
multivoltage <diego.tonini93@gmail.com>
2017-06-19 17:56:15 +0200

    add details component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 
 export default class App extends Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      entitySelected: null
+    }
+  }
+
   render() {
     return (
       <HashRouter>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/entities/:number' component={EntityDetails} />
+          <Route path='/entities/:number' render={() => <EntityDetails entityId="1"/>} />
         </Switch>
       </HashRouter>
     );
--- src/components/entity-details.js
+++ src/components/entity-details.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+const URL_API = 'https://raw.githubusercontent.com/multivoltage/track-board/master/src/data.json';
 
 export default class EntityDetails extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      entity: null
+    };
+  }
+
+  componentDidMount(){
+    fetch(URL_API)
+      .then((res) => res.json())
+      .then((dataObj) => {
+        //console.log(dataObj);
+        let entity = dataObj.entities.find((e) => e.id === this.props.entityId);
+        this.setState({
+          entity : entity
+        });
+        console.log(dataObj);
+        console.log(entity);
+      })
+      .catch((e) => {
+        console.error(e);
+      })
+  }
+
   render() {
+    console.log(this.state);
       return (
         <div className="entitiy-details">
           <Link to='/'>
             <span className="close">X</span>
+            <span>entity: </span>
           </Link>
         </div>
       );
