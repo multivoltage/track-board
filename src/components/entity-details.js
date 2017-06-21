e77517e59263af41dc2bee1a3102f8c1d535d014
multivoltage <diego.tonini93@gmail.com>
2017-06-21 17:46:57 +0200

    use descrpiton as defautl selected tab + style

--- src/components/entity-details.js
+++ src/components/entity-details.js
@@ -49,7 +49,9 @@ export default class EntityDetails extends Component {
             </header>
             <Divider />   
             <TabsLayout tabs={["Description","Flow","History"]} selected="Description">
-              <Button tab="Description">tab1</Button>
+              <Container className="description"  tab="Description">
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+              </Container>
               <Button tab="Flow">tab2</Button>
               <Button tab="History">tab3</Button>
             </TabsLayout>
